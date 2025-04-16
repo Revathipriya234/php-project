a35d97b6ef44aa842f3c8591cb8efb13ae123ed4
Revathipriya234 <revathipriyaredrouthu@gmail.com>
2025-04-16 11:25:12 +0530

    if-elseif using marks

---
+++ if-elseif.php
@@ -0,0 +1,14 @@
+<?php
+$marks=85;
+if($marks>=90){
+    echo "Excellent!";
+}elseif($marks>=80){
+    echo " very Good!";
+}elseif($marks>=60){
+    echo "Good!";
+}elseif($marks>=40){
+    echo "Passed!";
+}else{
+    echo "Failed";
+}
+?>
