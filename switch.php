f47aad032a5f0c6dee0a6a358eb2a0b822fc8784
Revathipriya234 <revathipriyaredrouthu@gmail.com>
2025-04-16 11:38:06 +0530

    switch exercise

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+$favColor = "red";
+switch($favColor){
+    case "red":
+        echo "My favorite color is red!";
+        break;
+    case "blue":
+        echo "My favorite color is blue!";
+        break;
+    case "green":
+        echo "My favorite color is green!";
+        break;
+    default:
+    echo "My favorite color is neither red,blue,nor green!";
+}
+?>
