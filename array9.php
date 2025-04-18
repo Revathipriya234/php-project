221c0425cb717fd604fa9fceb46b721810a85c10
Revathipriya234 <revathipriyaredrouthu@gmail.com>
2025-04-18 12:35:07 +0530

    multidimentional array3

---
+++ array9.php
@@ -0,0 +1,21 @@
+<?php
+$marks=array(
+    "Ankit"=>array(
+       "C"=>95,
+       "DCO"=>85,
+       "FOL"=>74,
+    ),
+    "Ram"=>array(
+       "C"=>78,
+       "DCO"=>98,
+       "FOL"=>46,
+    ),
+    "Anoop"=>array(
+        "C"=>88,
+        "DCO"=>46,
+        "FOL"=>99,
+    ),
+);
+echo "Display Marks:\n";
+print_r($marks);
+?>
